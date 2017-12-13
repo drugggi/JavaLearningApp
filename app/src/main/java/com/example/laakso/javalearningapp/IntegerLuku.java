6f160211525225267c1c42a4a1858afb828ec3a9
Kalle Laakso <laakso29@luukku.com>
2017-12-13 12:25:40 +0200

    Added Multiplication test in IntegerLuku

--- app/src/main/java/com/example/laakso/javalearningapp/IntegerLuku.java
+++ app/src/main/java/com/example/laakso/javalearningapp/IntegerLuku.java
@@ -8,26 +8,67 @@ package com.example.laakso.javalearningapp;
 
 public class IntegerLuku extends Luku {
 
+    private enum operandi {JAKO, MOD, KERTO}
+
     @Override
     String Muunna() {
-        boolean jako = super.rng.nextBoolean();
-        int n = super.rng.nextInt(100);
-        int m = super.rng.nextInt(50);
+
+
+        switch(randomOperandi() ) {
+            case JAKO:
+                return jakoOperandi();
+            case MOD:
+                return modOperandi();
+            case KERTO:
+                return kertoOperandi();
+            default:
+                return null;
+        }
+
+    }
+
+    private String kertoOperandi() {
+        int n = super.rng.nextInt(128);
+        int m = super.rng.nextInt(128);
         int ans;
         String problem;
 
-        if (jako) {
-            ans = n/m;
-            problem = "int ans = " + n + "/" + m +";\n" + "What value holds?";
-            super.answer = "" + ans;
-        }
-        else {
-            ans = n%m;
-            problem = "int ans = " + n + "%" + m +";\n" + "What value ans holds?";
-            super.answer = "" + ans;
-        }
+        ans = n*m;
+        problem = "int ans = " + n + "*" + m +";\n" + "What value ans holds?";
+        super.answer = "" + ans;
 
         return problem;
+    }
+
+    private String modOperandi() {
+        int n = super.rng.nextInt(128);
+        int m = super.rng.nextInt(64);
+        int ans;
+        String problem;
+
+        ans = n%m;
+        problem = "int ans = " + n + "%" + m +";\n" + "What value ans holds?";
+        super.answer = "" + ans;
+
+        return problem;
+    }
+
+    private String jakoOperandi() {
+        int n = super.rng.nextInt(128);
+        int m = super.rng.nextInt(64);
+        int ans;
+        String problem;
+
+        ans = n/m;
+        problem = "int ans = " + n + "/" + m +";\n" + "What value holds?";
+        super.answer = "" + ans;
+
+        return problem;
+    }
+
+    private operandi randomOperandi() {
+        int pick = super.rng.nextInt(operandi.values().length);
+        return operandi.values()[pick];
     }
 
     @Override
